d7ef12c4aea8802247563a2dcde47f8077c3de6f
RakeshDevopsPy <RakeshKombali@gmail.com>
2020-03-15 16:34:35 +0530

    Bash scripting file1

---
+++ Bash/Reading_Varaibles.sh
@@ -0,0 +1,9 @@
+echo "Enter the name: "
+read Name1,Name2,Name3,Name4
+echo "Entered the Name: $Name1,$Name2,$Name3,$Name4"
+
+read -p 'Username :' $Name5
+read -sp 'Password :' $Name6
+echo 
+echo "Username : $Name5"
+echo "Password : $Name6"
---
+++ Bash/helloWorld.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+#comment on the hello world program
+echo "hello world..." # this is also comment 
+echo my shell name is $BASH
+echo my shell version is #BASH VERSION
+echo my home directory is $HOME
+echo my current working directory is $PWD
+
+firstName=Rakesh
+lastName=Kombali
+value=10
+echo value is $value
+echo my first name is $firstName
+echo my last name is $lastName
